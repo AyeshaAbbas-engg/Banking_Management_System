d969f440e7025e1acd4f767a8856dc4c2fcd9719
AyeshaAbbas-engg <2024CS193@student.uet.edu.pk>
2025-05-09 21:06:54 +0500

    Save local changes before pulling

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ManagerDashBoard());
+            Application.Run(new EmployeedashBoard());
         }
     }
 }
